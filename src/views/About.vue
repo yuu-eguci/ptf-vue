d150c9c0ee7fb325b8285b72ba7b77cdbd7a37dc
yuu-eguci <yuu.eguci@gmail.com>
2020-03-24 22:19:14 +0900

    :globe_with_meridians: Translate with pluralization

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>{{$t('about.message')}}</h1>
+    <h2>{{$tc('about.wolves', 0)}}</h2>
+    <h2>{{$tc('about.wolves', 1)}}</h2>
+    <h2>{{$tc('about.wolves', 5)}}</h2>
   </div>
 </template>
