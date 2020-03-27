a0c04b83f58dcc51aa92f938ddf3727a1058b15e
yuu-eguci <yuu.eguci@gmail.com>
2020-03-27 15:37:13 +0900

    :art: Use @ for src path

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="@/assets/logo.png">
     <HelloWorld :msg="$t('home.message')"/>
   </div>
 </template>
