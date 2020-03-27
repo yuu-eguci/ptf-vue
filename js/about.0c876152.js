55f235df16ed96748164dfd8567904a94f431cf5
yuu-eguci <yuu-eguci@users.noreply.github.com>
2020-03-27 07:47:11 +0000

    deploy: 515bfd675dc57e725277b07dc1fdc0750ef1c591

--- js/about.0c876152.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,s,e){"use strict";e.r(s);var a=function(){var t=this,s=t.$createElement,e=t._self._c||s;return e("div",{staticClass:"about"},[e("h1",[t._v(t._s(t.$t("about.message")))]),e("h2",[t._v(t._s(t.$tc("about.wolves",0)))]),e("h2",[t._v(t._s(t.$tc("about.wolves",1)))]),e("h2",[t._v(t._s(t.$tc("about.wolves",5)))])])},o=[],u=e("2877"),l={},n=Object(u["a"])(l,a,o,!1,null,null,null);s["default"]=n.exports}}]);
-//# sourceMappingURL=about.0c876152.js.map
