a44480b8fbf93470c411a55ce42c172cd464f22b
yuu-eguci <yuu.eguci@gmail.com>
2020-03-29 17:24:02 +0900

    :heavy_plus_sign: Add BootstrapVue and vue-fontawesome

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import i18n from './i18n'
+import { BootstrapVue } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(fas, fab, far)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
