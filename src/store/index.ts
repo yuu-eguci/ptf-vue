a9c18c3f2b7ff413b1c05390aecc0ed77d70795d
yuu-eguci <yuu.eguci@gmail.com>
2020-03-31 17:11:54 +0900

    :heavy_plus_sign: Add vuex-persistedstate

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex, { StoreOptions } from 'vuex';
 import { RootState } from './types';
 import { auth } from '@/store/modules/auth'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -12,6 +13,11 @@ const store: StoreOptions<RootState> = {
   modules: {
     auth,
   },
+  plugins: [
+    createPersistedState({
+      storage: window.sessionStorage
+    })
+  ],
 }
 
 export default new Vuex.Store<RootState>(store)
